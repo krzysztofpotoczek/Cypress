d0535695708e3d3222ae3a0db189e9613d67aaea
”Potoczek_Krzysztof” <krzysztof.potoczek97@gmail.com>
2022-04-10 01:55:52 +0200

    Login test. Work but unstable.

--- cypress/integration/AllegroUI/login.spec.js
+++ cypress/integration/AllegroUI/login.spec.js
@@ -1,1 +1,48 @@
 /// <reference types="cypress" />
+
+describe('Otwieranie strony Allegro Snadbox', () => {
+    context('uruchomienie strony', () => {
+        it('Should open https://allegro.pl.allegrosandbox.pl/ ', () => {            
+            cy.visit('https://allegro.pl.allegrosandbox.pl/');             
+          })
+    })
+    
+
+    context('testy', () => {
+        it('Should accept popup', () => {          
+            cy.get('._jkrtd > .mgmw_9u', { timeout: 10000 }).should('be.visible').click();        
+            cy.wait(3000);
+          })
+    
+          it('Should login user', () => {          
+            cy.get('._13q9y > .mpof_5r_x', { timeout: 10000 }).should('be.visible').click();            
+            cy.get('._b4f07_1PpAM', { timeout: 10000 }).should('be.visible').click();   
+            cy.wait(3000);
+           })
+
+           it('Should accept popup', () => {               
+            cy.get('._jkrtd > .mgmw_9u', { timeout: 10000 }).should('be.visible').click();   
+            cy.wait(1000);         
+          
+           })
+
+           it('Should type user and password', () => {               
+            
+            cy.wait(1000);         
+            cy.get('#login', { timeout: 10000 }).should('be.visible').type('krzystukano+01@gmail.com');
+            cy.wait(1000); 
+            cy.get('#password', { timeout: 10000 }).should('be.visible').type('Tukano2020+');
+            
+           })
+
+           it('Should click login', () => {               
+           cy.get('.m7er_56', { timeout: 10000 }).should('be.visible').click(); 
+           })
+          })
+    })
+
+  
+
+     
+  
+ 
