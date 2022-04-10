c5929f7189ea678b5d62a3c4f64a71bddaff08dd
”Potoczek_Krzysztof” <krzysztof.potoczek97@gmail.com>
2022-04-10 02:08:09 +0200

    update

--- cypress/integration/AllegroUI/login.spec.js
+++ cypress/integration/AllegroUI/login.spec.js
@@ -11,28 +11,28 @@ describe('Otwieranie strony Allegro Snadbox', () => {
     context('testy', () => {
         it('Should accept popup', () => {          
             cy.get('._jkrtd > .mgmw_9u', { timeout: 10000 }).should('be.visible').click();        
-            cy.wait(3000);
+            cy.wait(2000);
           })
     
           it('Should login user', () => {          
             cy.get('._13q9y > .mpof_5r_x', { timeout: 10000 }).should('be.visible').click();            
             cy.get('._b4f07_1PpAM', { timeout: 10000 }).should('be.visible').click();   
-            cy.wait(3000);
+            cy.wait(2000);
            })
 
            it('Should accept popup', () => {               
             cy.get('._jkrtd > .mgmw_9u', { timeout: 10000 }).should('be.visible').click();   
-            cy.wait(1000);         
+            cy.wait(2000);         
           
            })
 
            it('Should type user and password', () => {               
             
-            cy.wait(1000);         
-            cy.get('#login', { timeout: 10000 }).should('be.visible').type('krzystukano+01@gmail.com');
-            cy.wait(1000); 
-            cy.get('#password', { timeout: 10000 }).should('be.visible').type('Tukano2020+');
-            
+            cy.wait(3000);         
+            cy.get('#login').type('test');
+            cy.wait(3000); 
+            cy.get('#password').type('test');
+            cy.wait(2000); 
            })
 
            it('Should click login', () => {               
